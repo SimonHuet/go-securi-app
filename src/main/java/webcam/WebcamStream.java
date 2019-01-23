ea1a3489d9d4793fc83c3e31948cecefda6847ff
unknown <simon.huet@epsi.fr>
2019-01-23 17:01:37 +0100

    Taking picture with webcam

---
+++ src/main/java/webcam/WebcamStream.java
@@ -0,0 +1,31 @@
+package webcam;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+
+public class WebcamStream {
+    public static void main (String[] args) throws IOException{
+
+        Dimension[] nonStandardResolutions = new Dimension[] {
+                WebcamResolution.PAL.getSize(),
+                WebcamResolution.HD.getSize(),
+                new Dimension(2000, 1000),
+                new Dimension(1000, 500),
+        };
+
+        Webcam webcam = Webcam.getDefault();
+
+        webcam.setCustomViewSizes(nonStandardResolutions);
+        webcam.setViewSize(WebcamResolution.HD.getSize());
+        webcam.open();
+
+        ImageIO.write(webcam.getImage(), "JPG", new File("webcaminput.jpg"));
+        webcam.close();
+    }
+}
