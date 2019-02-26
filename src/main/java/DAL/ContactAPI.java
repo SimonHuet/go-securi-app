8751d06ed110f96878039a15af77daf7f6af2b4e
unknown <simon.huet@epsi.fr>
2019-02-26 18:33:06 +0100

    Add head of bob : )

--- src/main/java/DAL/ContactAPI.java
+++ src/main/java/DAL/ContactAPI.java
@@ -5,10 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.FaceMatch;
-import com.amazonaws.services.rekognition.model.SearchFacesByImageRequest;
-import com.amazonaws.services.rekognition.model.Image;
-import com.amazonaws.services.rekognition.model.SearchFacesByImageResult;
+import com.amazonaws.services.rekognition.model.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,17 +21,21 @@ public class ContactAPI {
     public String getFaceId (Image image) throws JsonProcessingException {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJFEDG6WLUU5FJOHA", "OTkqjS4Gr1jKCMyX1rdcluTkVtATzQctg2Ncibcu");
         AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion(Regions.EU_WEST_1).build();
+
         SearchFacesByImageResult searchFacesByImageResult =
                 rekognitionClient.searchFacesByImage(createSearchFace(image));
         FaceMatch faceImageMatche = searchFacesByImageResult.getFaceMatches().get(0);
+
         if(!similarity(faceImageMatche)){
             throw  new Error("Pas de ressemblance");
         }
+        System.out.println(faceImageMatche.getFace().getFaceId());
         return faceImageMatche.getFace().getFaceId();
+
     }
 
     private SearchFacesByImageRequest createSearchFace(Image image) {
-        return new SearchFacesByImageRequest().withCollectionId("").withImage(image).withMaxFaces(1);
+        return new SearchFacesByImageRequest().withCollectionId("test").withImage(image).withMaxFaces(1);
     }
 
     private boolean similarity (FaceMatch face ){
